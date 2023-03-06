08f286c2d24a52cf05bb8c1bc8fd13fb9e81411b
belousov <belousovsergej56@gmail.com>
2023-03-07 00:10:23 +0300

    add docstrig, rename module to read_file

--- read_file.py
+++ read_file.py
@@ -1,9 +1,16 @@
+"""
+Module for convert and read files.
+
+Module have a func for convert xls to json,
+and read json for returns ditionary.
+"""
+
 import pandas as pd
 import json
 
 
 def xls_to_json(xls_file):
-    """Convert excel file to json
+    """Convert excel file to json.
 
     Args:
         xls_file (file.xlsx): excel file to be converted to json
@@ -13,7 +20,7 @@ def xls_to_json(xls_file):
 
 
 def read_json() -> dict:
-    """Reading a file.
+    """Read a file.
 
     The function reads a json file and returns a dictionary.
 
