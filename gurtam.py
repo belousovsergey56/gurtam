affd88cd96d72a99dbe4268c4a5030fd1671684c
belousov <belousovsergej56@gmail.com>
2023-03-06 23:53:16 +0300

    upd docstring, upd too long lines, remove spaces, create test func for error log

--- gurtam.py
+++ gurtam.py
@@ -1,12 +1,16 @@
+"""Base func for work with Gurtam API."""
 import requests
 import random
 import json
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 # https://fms4.csat.ru/?login&token=3469b1a0dfc158c6d35ea9ca7475238b68DB86E7A5D10C1466D4827A7F483E85A750D6A5
 
-URL = 'https://fms4.csat.ru/wialon/ajax.html'
-TOKEN = '3469b1a0dfc158c6d35ea9ca7475238b68DB86E7A5D10C1466D4827A7F483E85A750D6A5'
+URL = os.getenv('URL')
+TOKEN = os.getenv('TOKEN')
 CARKADE = {  # 0 - легковые, 1 - грузовые, 2 - спец.техника, 9 - рисковые
     '1': 40188,
     '0': 40187,
@@ -27,9 +31,9 @@ EVO = {
 
 
 def get_header() -> dict:
-    """Getting the User Agent.
+    """Get the User Agent.
 
-    The function has a list of User Agents, chooses a random 
+    The function has a list of User Agents, chooses a random
     one from the list and returns a dictionary.
 
     Returns:
@@ -37,7 +41,6 @@ def get_header() -> dict:
     """
     user_agent_list = [
         'Safari/537.36 OPR/26.0.1656.60',
-        'Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 '
         '(maverick) Firefox/3.6.10',
         'Chrome/39.0.2171.71 Safari/537.36',
         'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 '
@@ -49,12 +52,11 @@ def get_header() -> dict:
 
 
 def get_ssid() -> str:
-    """Authorization by token and getting the session number.
+    """Authorize by token and getting the session number.
 
-    The function authorizes with a token on fms4, receives a json file in response, 
-    from where it takes eid and returns it as a string, 
-    The function authorizes with a fms4 token, receives a json file in response, 
-    from where it takes eid and returns it as a string
+    The function authorizes with a token on fms4,
+    receives a json file in response,
+    from where it takes eid and returns it as a string,
 
     Returns:
         str: eid
@@ -68,7 +70,7 @@ def get_ssid() -> str:
 
 
 def search_object(ssid: str, imei: str) -> int:
-    """Finds object by imei and returns object id.
+    """Find object by imei and returns object id.
 
     The function finds an object in the database by imei and returns id.
     If the object is not found, the function returns -1
@@ -90,7 +92,7 @@ def search_object(ssid: str, imei: str) -> int:
                 "sortType": "sys_name"
             },
             "force": 1,
-            "flags": 1,
+            "flags": 4611686018427387903,
             "from": 0,
             "to": 0
         }),
@@ -98,65 +100,106 @@ def search_object(ssid: str, imei: str) -> int:
     }
 
     response = requests.post(URL, params=param)
-    return response.json().get('items')[0].get('id') if response.json().get('items') != [] else -1
+    if response.json().get('items') != []:
+        return response.json().get('items')[0].get('id')
+    return -1
 
 
 def update_param(sid: str, unit_id: int, new_value: dict):
-    """Fills object fields with new parameters
+    """Fill object fields with new parameters.
 
-    The function accepts a session ID, an object ID, and a dictionary with data to fill in the required fields of the object.
-    The data is passed using the requests object's post method, the function returns nothing.
-    
+    The function accepts a session ID, an object ID,
+    and a dictionary with data to fill in the required fields of the object.
+    The data is passed using the requests object's post method,
+    the function returns nothing.
+
     Args:
         sid (str): session id
         unit_id (int): gurtam object id
         new_value (dict): dictionary with new params
-    """    
+    """
     CONTRACT_NAME = {
         'svc': 'item/update_name',
-        'params': json.dumps({"itemId": unit_id, "name": '{0}'.format(new_value.get('DL'))}),
+        'params': json.dumps({
+            "itemId": unit_id,
+            "name": '{0}'.format(new_value.get('DL'))}),
         'sid': sid
     }
 
     IMEI = {
         'svc': 'item/update_admin_field',
-        'params': json.dumps({"itemId": unit_id, "id": 1, "callMode": 'update', "n": 'geozone_imei', "v": '{0}'.format(new_value.get('IMEI'))}),
+        'params': json.dumps({
+            "itemId": unit_id,
+            "id": 1,
+            "callMode": 'update',
+            "n": 'geozone_imei',
+            "v": '{0}'.format(new_value.get('IMEI'))}),
         'sid': sid
     }
 
     SIM = {
         'svc': 'item/update_admin_field',
-        'params': json.dumps({"itemId": unit_id, "id": 2, "callMode": 'update', "n": 'geozone_sim', "v": '{0}'.format(new_value.get('SIM'))}),
+        'params': json.dumps({
+            "itemId": unit_id,
+            "id": 2,
+            "callMode": 'update',
+            "n": 'geozone_sim',
+            "v": '{0}'.format(new_value.get('SIM'))}),
         'sid': sid
     }
 
     VIN = {
         'svc': 'item/update_custom_field',
-        'params': json.dumps({'itemId': unit_id, 'id': 1, 'callMode': 'update', 'n': 'Vin', 'v': '{0}'.format(new_value.get('VIN'))}),
+        'params': json.dumps({
+            'itemId': unit_id,
+            'id': 1,
+            'callMode': 'update',
+            'n': 'Vin',
+            'v': '{0}'.format(new_value.get('VIN'))}),
         'sid': sid
     }
 
     INFO4 = {
         'svc': 'item/update_admin_field',
-        'params': json.dumps({"itemId": unit_id, "id": 6, "callMode": 'update', "n": 'Инфо4', "v": '{0}'.format(new_value.get('INFO4'))}),
+        'params': json.dumps({
+            "itemId": unit_id,
+            "id": 6,
+            "callMode": 'update',
+            "n": 'Инфо4',
+            "v": '{0}'.format(new_value.get('INFO4'))}),
         'sid': sid
     }
 
     BRAND = {
         'svc': 'item/update_custom_field',
-        'params': json.dumps({'itemId': unit_id, 'id': 2, 'callMode': 'update', 'n': 'Марка', 'v': '{0}'.format(new_value.get('BRAND'))}),
+        'params': json.dumps({
+            'itemId': unit_id,
+            'id': 2,
+            'callMode': 'update',
+            'n': 'Марка',
+            'v': '{0}'.format(new_value.get('BRAND'))}),
         'sid': sid
     }
 
     MODEL = {
         'svc': 'item/update_custom_field',
-        'params': json.dumps({'itemId': unit_id, 'id': 3, 'callMode': 'update', 'n': 'Модель', 'v': '{0}'.format(new_value.get('MODEL'))}),
+        'params': json.dumps({
+            'itemId': unit_id,
+            'id': 3,
+            'callMode': 'update',
+            'n': 'Модель',
+            'v': '{0}'.format(new_value.get('MODEL'))}),
         'sid': sid
     }
 
     PIN = {
         'svc': 'item/update_admin_field',
-        'params': json.dumps({"itemId": unit_id, "id": 7, "callMode": 'update', "n": 'Пин', "v": '{0}'.format(new_value.get('PIN'))}),
+        'params': json.dumps({
+            "itemId": unit_id,
+            "id": 7,
+            "callMode": 'update',
+            "n": 'Пин',
+            "v": '{0}'.format(new_value.get('PIN'))}),
         'sid': sid
     }
 
@@ -171,10 +214,33 @@ def update_param(sid: str, unit_id: int, new_value: dict):
         'params': json.dumps({'itemId': unit_id, 'newValue': 0}),
         'sid': sid
     }
-    
-    param_list = [CONTRACT_NAME, IMEI, SIM, VIN, INFO4, BRAND, MODEL, PIN, DISTANCE, ENGIN_HOURS]
+
+    param_list = [CONTRACT_NAME, IMEI, SIM, VIN, INFO4,
+                  BRAND, MODEL, PIN, DISTANCE, ENGIN_HOURS]
+
     for param in param_list:
-        requests.post(URL, params=param)
+        a = requests.post(URL, params=param)
+        get_log(a, param, new_value)
+
+
+def get_log(response, param, value):
+    """_summary_
+
+    Args:
+        response (_type_): _description_
+        param (_type_): _description_
+        value (_type_): _description_
+    """
+    log_text = 'Запись параметра: {0} - объекта:\
+        {1} - завершена с ошибкой. Код ошибки - {2} \n'
+    if response.status_code != 200:
+        with open('log.txt', 'a') as log:
+            log.write(
+                log_text.format(
+                    param.get('svc'),
+                    value.get('DL'),
+                    response.text)
+            )
 
 
 def search_groups_by_name(ssid: str, group_name: str) -> dict:
@@ -186,7 +252,7 @@ def search_groups_by_name(ssid: str, group_name: str) -> dict:
 
     Returns:
         dict: _description_
-    """    
+    """
     param = {
         "svc": "core/search_items",
         "params": json.dumps({
@@ -229,39 +295,46 @@ def search_group_by_id(ssid: str, group_id: int) -> dict:
 
 
 def id_group(group: dict) -> int | str:
-    """Getting ID group
+    """Get ID group.
 
     Args:
         group (dict): json object with meta data about group
 
     Returns:
-        int|str: if group finded, return group id as integer, if not finded return traceback 'this is group is not finded'
+        int|str: if group finded, return group id as integer,
+        if not finded return traceback 'this is group is not finded'
     """
     try:
         return group['items'][0].get('id')
-    except IndexError as e:
+    except IndexError:
         return 'Группа не найдена'
 
 
 def group_unit_list(group: dict) -> list | str:
-    """Getting group unit list
+    """Get group unit list.
 
     Args:
         group (dict): json object with meta data about group
 
     Returns:
-        list|str: if group finded, return unit list as [123, 456, ...etc], if not finded return traceback 'this is list empty'
+        list|str: if group finded, return unit list as [123, 456, ...etc],
+        if not finded return traceback 'this is list empty'
     """
     try:
         if 'items' in group:
             return group['items'][0].get('u')
         else:
             return group['item'].get('u')
-    except IndexError as e:
+    except IndexError:
         return 'Список пуст'
 
 
-def add_groups(ssid: str, leasing_id: int | str, leasing_unit_list: list[int], added_unit: list[int]):
+def add_groups(
+    ssid: str,
+    leasing_id: int | str,
+    leasing_unit_list: list[int],
+    added_unit: list[int]
+):
     """_summary_
 
     Args:
@@ -272,13 +345,20 @@ def add_groups(ssid: str, leasing_id: int | str, leasing_unit_list: list[int], a
     """
     param = {
         'svc': 'unit_group/update_units',
-        'params': json.dumps({"itemId": leasing_id, "units": leasing_unit_list + added_unit}),
+        'params': json.dumps({
+            "itemId": leasing_id,
+            "units": leasing_unit_list + added_unit}),
         'sid': ssid
     }
     requests.post(URL, params=param)
 
 
-def remove_groups(ssid: str, leasing_id: int | str, leasing_unit_list: list[int], removed_unit: list[int]):
+def remove_groups(
+    ssid: str,
+    leasing_id: int | str,
+    leasing_unit_list: list[int],
+    removed_unit: list[int]
+):
     """_summary_
 
     Args:
@@ -291,7 +371,9 @@ def remove_groups(ssid: str, leasing_id: int | str, leasing_unit_list: list[int]
         leasing_unit_list.remove(unit)
     param = {
         'svc': 'unit_group/update_units',
-        'params': json.dumps({"itemId": leasing_id, "units": leasing_unit_list}),
+        'params': json.dumps({
+            "itemId": leasing_id,
+            "units": leasing_unit_list}),
         'sid': ssid
     }
 
@@ -299,13 +381,15 @@ def remove_groups(ssid: str, leasing_id: int | str, leasing_unit_list: list[int]
 
 
 if __name__ == '__main__':
-
+    # test id object [50597, 53143, 53209]
     sid = get_ssid()
-    a = search_object(imei='401010053722801721', ssid=sid)    
+    # a = search_object(imei='401010053722801721', ssid=sid)
+    # b = search_groups_by_name(sid, 'belousov')
+    print()
     json_ = [{
         'DL': 'belousov_belousov',
         'IMEI': '401010053722801721',
-        'SIM': '+79117084809',
+        'SIM': '+79117084888',
         'VIN': '0DLGRT124566IO90',
         'INFO4': '',
         'BRAND': 'OMODA',
@@ -316,8 +400,5 @@ if __name__ == '__main__':
         'LEASING': 'belousov_13',
         'CONFIG': 'FMB130',
     }]
-    
-    # update_param(sid, a, json_[0])
-    
-    a = requests.post(URL)
-    print(a.status_code == 200)
+
+    b = update_param(sid, 50597, json_[0])
