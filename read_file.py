c22646d6ba3b7782df6e8e826157f5509e85a449
belousov <belousovsergej56@gmail.com>
2023-03-10 00:27:38 +0300

    upd docstring, path to store the file

--- read_file.py
+++ read_file.py
@@ -2,7 +2,7 @@
 Module for convert and read files.
 
 Module have a func for convert xls to json,
-and read json for returns ditionary.
+and read json for returns dictionary.
 """
 
 import pandas as pd
@@ -10,23 +10,24 @@ import json
 
 
 def xls_to_json(xls_file):
-    """Convert excel file to json.
+    """Convert Excel file to json.
 
     Args:
-        xls_file (file.xlsx): excel file to be converted to json
+        xls_file (file.xlsx): Excel file to be converted to json
     """
     xls = pd.read_excel(xls_file)
-    xls.to_json('work_file.json', orient='records')
+    xls.to_json('upload/work_file.json', orient='records')
 
 
 def read_json() -> dict:
     """Read a file.
 
-    The function reads a json file and returns a dictionary.
+    The function reads a json file saved in upload/ directory
+    and returns a dictionary.
 
     Returns:
         dict: dictionary ready to use
     """
-    with open('work_file.json', 'r') as f:
+    with open('upload/work_file.json', 'r', encoding='UTF-8') as f:
         a = json.loads(f.read())
         return a
