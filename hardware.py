3631b80d52e157770801d89ea26885627e5ae05b
belousov <belousovsergej56@gmail.com>
2023-03-09 09:21:43 +0300

    upd docstring

--- hardware.py
+++ hardware.py
@@ -9,12 +9,12 @@ HW_ID = {'MT5': 20, 'FMB130': 19510,
          'FMB125': 96, 'FMB920': 69, 'FMB140': 19511}
 
 
-def create_object(sid: str, hardware: int, object_name: str):
+def create_object(sid: str, hardware: str, object_name: str):
     """Create new object.
 
     Args:
         sid (str): Session id
-        hardware (int): id hardware
+        hardware (str): id hardware
         object_name (str): this is contract name
     """
     params = {
