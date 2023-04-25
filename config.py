26294edc4f9f7c18fc88c4b9bda2ec42cf27422e
belousovsergey56 <belousovsergej56@gmail.com>
2023-04-25 13:44:26 +0300

    add config file

--- config.py
+++ config.py
@@ -1,34 +1,34 @@
-# """Config module.
+"""Config module.
 
-# This module have all settings for Flask, SQLAlchemy, Login manager.
-# """
-# import os
+This module have all settings for Flask, SQLAlchemy, Login manager.
+"""
+import os
 
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# from flask import Flask
+from flask import Flask
 
-# from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap
 
-# from flask_ckeditor import CKEditor
+from flask_ckeditor import CKEditor
 
-# from flask_login import LoginManager
+from flask_login import LoginManager
 
-# from flask_sqlalchemy import SQLAlchemy
-
-
-# load_dotenv()
-# db_password = os.getenv('mysql_password_root')
+from flask_sqlalchemy import SQLAlchemy
 
 
-# app = Flask(__name__)
-# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-# Bootstrap(app)
+load_dotenv()
+db_password = os.getenv('mysql_password_root')
 
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///leasing_users.db"
-# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-# login_manager = LoginManager()
-# login_manager.init_app(app)
-# ckeditor = CKEditor(app)
-# db = SQLAlchemy(app)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
+Bootstrap(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///leasing_users.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+ckeditor = CKEditor(app)
+db = SQLAlchemy(app)
