b6ab4bf3ceeae99443bba1cdc5d691428e7d652a
belousovsergey56 <belousovsergej56@gmail.com>
2023-06-13 00:28:10 +0300

    upd User model, add access field and group

--- models.py
+++ models.py
@@ -1,8 +1,5 @@
 """Models data base leasing users."""
-from config import app, db, admin
-from flask_admin.contrib.sqla import ModelView
-
-from werkzeug.security import generate_password_hash
+from config import app, db
 
 from flask_login import UserMixin
 
@@ -22,24 +19,13 @@ class User(UserMixin, db.Model):
     login = db.Column(db.String(1000))
     email = db.Column(db.String(1000), unique=True)
     password = db.Column(db.String(1000))
-
-    def __init__(self, login, email, password):
-        self.login = login
-        self.email = email
-        self.password = password
-
-
-class AdminView(ModelView):
-    def on_model_change(self, form, model, is_created):
-        if 'password' in form:
-            model.password = generate_password_hash(form.password.data)
-        super(AdminView, self).on_model_change(form, model, is_created)
-    form_widget_args = {
-        'password': {'type': 'password'}
-    }
+    group = db.Column(db.String(100))
+    access_create = db.Column(db.Boolean)
+    access_remove = db.Column(db.Boolean)
+    access_edit = db.Column(db.Boolean)
 
 
-admin.add_view(AdminView(User, db.session))
+
 
 with app.app_context():
     db.create_all()
