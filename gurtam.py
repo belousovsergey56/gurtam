8c16e8788891f7c4778db3eae87a338874d2f8d1
belousovsergey56 <belousovsergej56@gmail.com>
2024-03-12 22:26:09 +0300

    in group_update, added update required groups, add const tuple with id required groups

--- gurtam.py
+++ gurtam.py
@@ -23,6 +23,7 @@ AUTO = (40187, 41342, 40576)
 TRUCK = (40188, 41343, 40577)
 SPEC = (53014, 53012, 53013)
 RISK = (40166, 59726)
+REQUIRED_GROUPS = (78875)
 CUSTOM_FIELDS = ('Vin', 'Марка', 'Модель')
 
 
@@ -701,6 +702,9 @@ def group_update(data: dict) -> None:
             add_groups(sid, id_group, leasing_unit_list, risk_auto)
         else:
             add_groups(sid, id_group, leasing_unit_list, all_unit)
+    for id_group in REQUIRED_GROUPS:
+        leasing_unit_list = group.get('u')
+        add_groups(sid, id_group, leasing_unit_list, all_unit)
     logger.debug('Объекты добавлены')
 
 
