b9a1140df1a97c8c4e36891d9cc772b5f7885d49
belousovsergey56 <belousovsergej56@gmail.com>
2024-06-25 22:11:10 +0300

    upd style: spaces, replace single quotes to duble quotes

--- config.py
+++ config.py
@@ -2,28 +2,23 @@
 
 This module have all settings for Flask, SQLAlchemy, Login manager.
 """
+
 import os
 
 from dotenv import load_dotenv
-
 from flask import Flask, request
-
 from flask_bootstrap import Bootstrap
-
 from flask_ckeditor import CKEditor
-
 from flask_login import LoginManager, current_user
-
 from flask_sqlalchemy import SQLAlchemy
-
 from loguru import logger
 
 load_dotenv()
-db_password = os.getenv('mysql_password_root')
+db_password = os.getenv("mysql_password_root")
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
+app.config["SECRET_KEY"] = os.getenv("FLASK_SECRET_KEY")
 Bootstrap(app)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///leasing_users.db"
@@ -36,16 +31,16 @@ db = SQLAlchemy()
 db.init_app(app)
 
 logger.add(
-    'logging/log_report.log',
-    format='{time:DD-MM-YYYY HH:mm:ss} - {level} - {name} - {module} - {message}',
+    "logging/log_report.log",
+    format="{time:DD-MM-YYYY HH:mm:ss} - {level} - {name} - {module} - {message}",
     rotation="60 days",
     compression="zip",
-    level="INFO"
-    )
+    level="INFO",
+)
 
 
 def log_message(message: str) -> str:
-    '''Template log message
+    """Template log message
 
     Args:
         ip (str): remote ip
@@ -55,18 +50,18 @@ def log_message(message: str) -> str:
 
     Return:
         str: log message in format 'ip - login - url - message'
-    '''
-    login = current_user.login if current_user.is_active else 'не известный',
-    url = request.base_url,
-    return f'{get_ip()} - {login[0]} - {url[0]} - {message}'
+    """
+    login = (current_user.login if current_user.is_active else "не известный",)
+    url = (request.base_url,)
+    return f"{get_ip()} - {login[0]} - {url[0]} - {message}"
 
 
 def get_ip() -> str:
-    '''Get remote ip.
+    """Get remote ip.
 
     Returns:
         str: remote ip
-    '''
+    """
     return request.remote_addr
 
 
@@ -76,4 +71,5 @@ def fstart_stop(func, *args, **kwargs):
         a = func(*args, **kwargs)
         logger.debug(f'окончание работы функции: "{func.__name__}"')
         return a
+
     return wraper
