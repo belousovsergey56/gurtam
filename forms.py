db171bab33fe0c06cf4e0f018e8971cd8654de97
belousovsergey56 <belousovsergej56@gmail.com>
2024-07-09 10:56:17 +0300

    upd imports, add to UploadFile "fms" field

--- forms.py
+++ forms.py
@@ -1,8 +1,16 @@
 """Module with forms for the application."""
 
+from constant import FMS
 from flask_wtf import FlaskForm
-from wtforms import FileField, SubmitField, PasswordField
-from wtforms import EmailField, StringField, BooleanField, SelectField
+from wtforms import (
+    BooleanField,
+    EmailField,
+    FileField,
+    PasswordField,
+    SelectField,
+    StringField,
+    SubmitField,
+)
 from wtforms.validators import DataRequired
 
 
@@ -13,8 +21,9 @@ class UploadFile(FlaskForm):
         FlaskForm (class): extends FlaskForm for create forms.
     """
 
+    fms = SelectField(label="", render_kw={"placeholder": "FMS"}, choices=FMS)
     export_file = FileField(validators=[DataRequired()])
-    submit = SubmitField('Экспорт')
+    submit = SubmitField("Экспорт")
 
 
 class SigninForm(FlaskForm):
@@ -25,16 +34,20 @@ class SigninForm(FlaskForm):
     """
 
     login = StringField(
-        label='',
-        render_kw={'placeholder': 'Логин'},
-        validators=[DataRequired()]
+        label="",
+        render_kw={"placeholder": "Логин"},
+        validators=[
+            DataRequired(),
+        ],
     )
     password = PasswordField(
-        label='',
-        render_kw={'placeholder': 'Пароль'},
-        validators=[DataRequired()]
+        label="",
+        render_kw={"placeholder": "Пароль"},
+        validators=[
+            DataRequired(),
+        ],
     )
-    submit = SubmitField('Войти')
+    submit = SubmitField("Войти")
 
 
 class UserForm(FlaskForm):
@@ -45,25 +58,27 @@ class UserForm(FlaskForm):
     Args:
         FlaskForm (class): extends FlaskForm for create forms.
     """
+
     login = StringField(
-        label='',
-        render_kw={'placeholder': 'Логин'},
-        validators=[DataRequired()]
+        label="",
+        render_kw={"placeholder": "Логин"},
+        validators=[
+            DataRequired(),
+        ],
     )
     email = EmailField(
-        label='',
-        render_kw={'placeholder': 'Адрес электронной почты'},
-        validators=[DataRequired()]
+        label="",
+        render_kw={"placeholder": "Адрес электронной почты"},
+        validators=[DataRequired()],
     )
     password = PasswordField(
-        label='',
-        render_kw={'placeholder': 'Пароль'},
-        validators=[DataRequired()]
+        label="",
+        render_kw={"placeholder": "Пароль"},
+        validators=[
+            DataRequired(),
+        ],
     )
-    group = SelectField(
-        label='',
-        render_kw={'placeholder': 'Выбрать лизинг'}
-        )
-    access_create = BooleanField(label='Создание')
-    access_remove = BooleanField(label='Удаление')
-    access_edit = BooleanField(label='Изменение')
+    group = SelectField(label="", render_kw={"placeholder": "Выбрать лизинг"})
+    access_create = BooleanField(label="Создание")
+    access_remove = BooleanField(label="Удаление")
+    access_edit = BooleanField(label="Изменение")
